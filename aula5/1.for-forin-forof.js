8acfe2ea7e781faa3699149833e3638cbd3277e0
Lucas Gabriel <lucas.gabriel@deway.com.br>
2019-10-07 15:24:43 -0300

    aula5

---
+++ aula5/1.for-forin-forof.js
@@ -0,0 +1,32 @@
+const service = require('./service')
+
+async function main() {
+    try {
+        const result = await service.getPeople('a');
+        const names = [];
+        console.time('for')
+        for(let i=0; i <= result.results.length - 1; i++) {
+            const people = result.results[i]
+            names.push(people.name)
+        }
+        console.timeEnd('for')
+        console.time('forin')
+        for (let i in result.results) {
+            const people = result.results[i]
+            names.push(people.name)
+        }
+        console.timeEnd('forin')
+        console.time('forof')
+        for (person of result.results) {
+            names.push(person.name)
+        }
+        console.timeEnd('forof')
+
+        console.log('names', names)
+    }
+    catch(error) {
+        console.error('internal error', error)
+    }
+}
+
+main()
